ee4079d50f53b374289094f4151372ea989d7b5a
dk19999 <rattandeepak1999@gmail.com>
2024-02-26 00:13:54 +0530

    Add singly linked list implementation

---
+++ linked-list/singly-linked-list.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+}
+
+const l1 = new LinkedList();
+
+l1.append(1);
+
+l1.append(2);
+
