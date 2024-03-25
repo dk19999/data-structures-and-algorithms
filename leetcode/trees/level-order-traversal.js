6d4e6055f4a013e9698bc292c906f23ed503e3bb
dk19999 <rattandeepak1999@gmail.com>
2024-03-25 23:54:30 +0530

    Add interative solution for binary level order traversal

--- leetcode/trees/level-order-traversal.js
+++ leetcode/trees/level-order-traversal.js
@@ -27,3 +27,47 @@ var levelOrder = function (root) {
 
   return result;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+var levelOrderIteratively = function (root) {
+  if (!root) {
+    return [];
+  }
+  let queue = [root];
+  let result = [];
+
+  while (queue.length) {
+    const currentLevelNodes = [];
+
+    const queueLength = queue.length;
+
+    for (let i = 0; i < queueLength; i++) {
+      const node = queue.shift();
+
+      currentLevelNodes.push(node.val);
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    result.push(currentLevelNodes);
+  }
+
+  return result;
+};
